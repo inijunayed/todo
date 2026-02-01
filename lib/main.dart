0ca9e6d4c915531b9892523548e2fbc8dd33aec5
inijunayed <ini.junayed@gmail.com>
2026-02-01 16:26:58 +0600

    improved

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:todo/src/home/home.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
--- lib/src/home/view_model/home_view_model.dart
+++ lib/src/home/view_model/home_view_model.dart
@@ -1,15 +1,34 @@
 import 'dart:developer';
 
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:todo/src/models/todo_model.dart';
 
 class HomeViewModel extends GetxController {
   var todos = <TodoModel>[
     TodoModel(title: "title", description: "description", isComplete: false),
   ].obs;
+  final box = GetStorage();
+  final String _key = "todoItems";
+  void storeData() {
+    box.write(_key, todos);
+  }
+
+  void getData() {
+    final myTodos = box.read(_key);
+
+    log(myTodos.toString());
+  }
+
+  @override
+  void onInit() {
+    getData();
+    super.onInit();
+  }
 
   void addTodo(TodoModel todo) {
     todos.add(todo);
+    storeData();
     todos.refresh();
   }
 
